2aac303b62d653d5e2117ab5d54b62778bdaa933
Frank Rowe <fsrowe@gmail.com>
2013-06-05 17:20:15 -0400

    added definition + design update

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -1,11 +1,22 @@
 function Dashboard(){
+  var self = this;
   this.stat = 'Cover Crops';
   this.geo = 'Maryland';
   this.map = new Map();
   this.chart = new Chart();
   this.addHandlers();
   this.chart.makeLineChart();
-  this.getSocrataStat(this.stat, this.geo);
+  this.loadData(function(){
+    self.getSocrataStat(self.stat, self.geo);
+  });
+}
+
+Dashboard.prototype.loadData = function(next){
+  var self = this;
+  $.getJSON('data/stats.json', function(res){
+    self.statsData = res;
+    next();
+  });
 }
 
 Dashboard.prototype.getStats = function() {
@@ -19,16 +30,19 @@ Dashboard.prototype.getStats = function() {
 Dashboard.prototype.getSocrataStat = function(stat, geo){
   var self = this;
   $('#line-chart .title').html('<h5>' + stat + ' (' + geo + ')</h5>');
+  var def = _.where(self.statsData, {stat: stat})[0].definition;
+  $('#notes .inner').html('<div class="def"><h6>Definition:</h6><p>' + def + '</p></div>');
   $.getJSON('api/bay/stat/' + stat + '/' + geo, function(res){
     self.chart.updateChart(res);
   });
 }
 
 Dashboard.prototype.addHandlers = function(){
+  var self = this;
   $('a.stat').click(function(e){
     e.preventDefault();
     var stat = $(this).html();
-    dashboard.stat = $("<div/>").html(stat).text(); //decode
-    dashboard.getSocrataStat(dashboard.stat, dashboard.geo);
+    self.stat = $("<div/>").html(stat).text(); //decode
+    self.getSocrataStat(self.stat, self.geo);
   });
 }
