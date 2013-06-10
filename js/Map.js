00c0038b842c390da695d5bb0d1a321d6331400d
Frank Rowe <fsrowe@gmail.com>
2013-06-10 11:51:26 -0400

    removed max bounds from map

--- js/Map.js
+++ js/Map.js
@@ -1,6 +1,6 @@
 function Map(){
   this.map = new L.Map('map', {
-    minZoom: 7
+
   }).setView(new L.LatLng(38.85, -77.4), 7);
 
   var baseURL = 'http://a.tiles.mapbox.com/v3/esrgc.map-y9awf40v/{z}/{x}/{y}.png';
@@ -12,5 +12,5 @@ function Map(){
 
   L.tileLayer(countyURL).addTo(this.map);
 
-  this.map.setMaxBounds(this.map.getBounds());
+  //this.map.setMaxBounds(this.map.getBounds());
 }
