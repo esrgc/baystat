09bae7d3d8f1af861028accf48c986c3b3b8b2af
Frank Rowe <fsrowe@gmail.com>
2013-06-12 11:05:10 -0400

    removed base layer + map border

--- js/Map.js
+++ js/Map.js
@@ -1,14 +1,15 @@
 function Map(){
   this.map = new L.Map('map', {
-    attributionControl: false
+    attributionControl: false,
+    zoomControl: false
   }).setView(new L.LatLng(38.85, -77.4), 7);
 
   var baseURL = 'http://a.tiles.mapbox.com/v3/esrgc.map-y9awf40v/{z}/{x}/{y}.png';
   var countyURL = 'http://a.tiles.mapbox.com/v3/esrgc.CountyCompare/{z}/{x}/{y}.png';
 
-  L.tileLayer(baseURL, {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(this.map);
+  // L.tileLayer(baseURL, {
+  //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  // }).addTo(this.map);
 
   L.tileLayer(countyURL).addTo(this.map);
 
