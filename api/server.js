dc8c9079c796aabfe2b160ed10e919a8c4217f38
Frank Rowe <fsrowe@gmail.com>
2013-08-12 10:59:56 -0400

    refactor

--- api/server.js
+++ api/server.js
@@ -5,8 +5,8 @@ var request = require('request'),
     app = express(),
     config = JSON.parse(fs.readFileSync('config/config.json'));
 
-var dataurl = "https://data.maryland.gov/resource/8nvv-y5u6.json?$$app_token=" + config.app_token;
-var causes = {
+var solutions_url = "https://data.maryland.gov/resource/8nvv-y5u6.json?$$app_token=" + config.app_token;
+var causes_url = {
   'Nitrogen': "https://data.maryland.gov/resource/mp2x-4nn6.json?$$app_token=" + config.app_token,
   'Phosphorus': "https://data.maryland.gov/resource/hucz-vxqe.json?$$app_token=" + config.app_token,
   'Sediment': "https://data.maryland.gov/resource/bf9r-nark.json?$$app_token=" + config.app_token
@@ -22,7 +22,7 @@ app.get('/', function(req, res){
 app.get('/bay/stat/solutions/:stat?/:geo?', function(req, res){
   var geo = encodeURIComponent(req.params.geo),
       stat = encodeURIComponent(req.params.stat),
-      url = dataurl + "&$where=basinname='"+geo+"'%20and%20bmpname='"+stat+"'";
+      url = solutions_url + "&$where=basinname='"+geo+"'%20and%20bmpname='"+stat+"'";
   socrata(url, function(json){
     res.json(json);
   });
@@ -32,7 +32,7 @@ app.get('/bay/stat/causes/:pollution?/:source?/:geo?', function(req, res){
   var geo = encodeURIComponent(req.params.geo),
       source = encodeURIComponent(req.params.source),
       pollution = encodeURIComponent(req.params.pollution),
-      url = causes[pollution];
+      url = causes_url[pollution];
   
   if(req.params.geo === 'Maryland') {
     url += "&$select=sum(ms2013) as milestone2013,sum(_1985),sum(_2007),sum(_2009),sum(_2010),sum(_2011),sum(_2012)";
@@ -58,7 +58,7 @@ app.get('/bay/stat/causes/:pollution?/:source?/:geo?', function(req, res){
 app.get('/bay/stat/sources/:pollution?/:geo?', function(req, res){
   var pollution = encodeURIComponent(req.params.pollution),
       geo = encodeURIComponent(req.params.geo),
-      url = causes[pollution] + "&$select=sourcesector,sum(_2012)&$group=sourcesector";
+      url = causes_url[pollution] + "&$select=sourcesector,sum(_2012)&$group=sourcesector";
       if(req.params.geo !== 'Maryland') {
         url += "&$where=basinname='" + geo + "'";
       }
@@ -70,7 +70,7 @@ app.get('/bay/stat/sources/:pollution?/:geo?', function(req, res){
 
 
 app.get('/bay/stats', function(req, res){
-  var url = dataurl + "&$select=bmpname";
+  var url = solutions_url + "&$select=bmpname";
   socrata(url, function(json){
     var stats = _.pluck(json, 'bmpname');
     stats = _.uniq(stats);
