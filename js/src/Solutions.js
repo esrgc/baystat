b3755fb7a193be66c7c83a52223c1eeedcc7cbef
Frank Rowe <fsrowe@gmail.com>
2014-03-31 12:32:57 -0400

    change title

--- js/src/Solutions.js
+++ js/src/Solutions.js
@@ -1,7 +1,7 @@
 
 var SolutionsModel = Backbone.Model.extend({
   defaults: {
-    title: "Maryland's 2014 - 2015 Milestone Goals and Progress Reports",
+    title: "Maryland's 2014 - 2015 Milestone Goals and Progress Report",
     stat: 'Cover Crops',
     geo: 'Maryland',
     zoom: 7,
